a353173ecf9a7e254b3db1efce5c08f19321cfc4
antoine@thefurnitureguys.com <antoine@thefurnitureguys.ca>
2021-12-25 02:14:37 -0500

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+// + add the useEffect import
+import {useEffect, useState} from 'react';
+import {Button, Card, CardActions, CardContent} from "@material-ui/core";
 
+
+function MyText(prop) {
+  return <p style={{justifyContent: 'center', fontSize: '40px'}}>{prop.text}</p>
+}
+
+//
 function App() {
+  const [text, setText] = useState('');
+
+  async function nextQuestion() {
+    let pk = text.id
+    const requestOptions = {
+      method: 'PATCH',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({pose_question: false, internal_rating: text.internal_rating - 200})
+    };
+    let url = "http://10.0.0.102:8000/sentence/" + pk + "/"
+    fetch(url, requestOptions)
+      .then(response => response.json());
+
+    const response = await fetch("http://10.0.0.102:8000/sentence/random-sentence/");
+    const data = await response.json();
+    console.log(data)
+    // store the data into our books variable
+    setText(data);
+  }
+
+  async function confirm() {
+    let pk = text.id
+    const requestOptions = {
+      method: 'PUT',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({pose_question: true})
+    };
+    let url = "http://10.0.0.102:8000/sentence/" + pk + "/set-pose/"
+    fetch(url, requestOptions)
+      .then(response => response.json());
+
+    const response = await fetch("http://10.0.0.102:8000/sentence/random-sentence/");
+    const data = await response.json();
+    console.log(data)
+    // store the data into our books variable
+    setText(data);
+  }
+
+  // + adding the use
+  useEffect(() => {
+    getData();
+
+    // we will use async/await to fetch this data
+    async function getData() {
+      const response = await fetch("http://10.0.0.102:8000/sentence/random-sentence/");
+      const data = await response.json();
+      console.log(data)
+      // store the data into our books variable
+      setText(data);
+    }
+  }, []); // <- you may need to put the setBooks function in this array
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Card variant="outlined" sx={{maxWidth: 275}}>
+      <CardContent>
+        <div>
+          <h1>Card # {text.id} - rating {text.internal_rating}</h1>
+
+          <MyText text={text.text}/>
+        </div>
+      </CardContent>
+      <CardActions style={{justifyContent: 'center'}}>
+
+        <Button variant="contained" onClick={nextQuestion} size="large"><h1>Nope</h1></Button>
+        <Button variant="contained" onClick={confirm} size="large"><h1>Post</h1></Button>
+
+      </CardActions>
+    </Card>
+  )
 }
 
 export default App;
